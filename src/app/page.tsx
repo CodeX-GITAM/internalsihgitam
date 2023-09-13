df1deafe89c141a8de29cd4d5df47efae5d2b99e
breathecode6365 <pdandlur@gitam.in>
2023-09-13 18:32:20 +0530

    designs added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,18 +2,20 @@
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import axios from "axios";
-import banner from "../assets/banner.jpg"
+import banner from "../assets/banner.jpg";
+import Confetti from "@/components/Confetti";
+import Design from "@/components/Confetti";
 
 export default function Home() {
-  const startDate = new Date("2023-09-11T09:58:00"); // Set your desired start date and time
-  const targetDate = new Date("2023-09-12T10:58:00"); // Set your desired target date and time
+  const startDate = new Date("2023-09-14T10:00:00"); // Set your desired start date and time
+  const targetDate = new Date("2023-09-15T10:00:00"); // Set your desired target date and time
 
   // Initialize state for the remaining time
   const [remainingTime, setRemainingTime] = useState<{
     hours: number;
     minutes: number;
     seconds: number;
-  }>({ hours: 24, minutes: 59, seconds: 59 });
+  }>({ hours: 23, minutes: 59, seconds: 59 });
 
   // Initialize state for team data
   const [teamData, setTeamData] = useState<any[]>([]);
@@ -130,79 +132,103 @@ export default function Home() {
   ];
 
   return (
-    <main className=" ">
+    <main className="min-h-screen">
       <div className="mt-[65px] relative  w-screen h-[400px]">
         <Image
           src={banner}
           alt="banner"
-          className="absolute inset-0 w-screen h-[350px] blur-[2px]"
+          className="absolute inset-0 w-screen h-[350px] "
         />
         <div className="absolute inset-0 flex justify-center items-center">
-          <div className="flex flex-row gap-3 sp:w-50">
-            <div className="bg-slate-400 w-[100px] h-[150px] bg-opacity-75 flex flex-col justify-center space-y-3 items-center">
-              <h1 className="text-4xl font-bold">{remainingTime.hours}</h1>
-              <p className="font-semibold">hours</p>
+          {remainingTime.hours === 0 &&
+          remainingTime.minutes === 0 &&
+          remainingTime.seconds === 0 ? (
+            <div className="flex flex-col text-center">
+              <Design />
+              <h1 className="font-bold text-white text- text-8xl font-sans">
+                TIME OVER!!
+              </h1>
+              <h1 className="font-bold text-white">
+                Congratulations on Completion!!!
+              </h1>
             </div>
-            <div className="bg-slate-400 w-[100px] h-[150px] bg-opacity-75 flex flex-col justify-center space-y-3 items-center">
-              <h1 className="text-4xl font-bold">{remainingTime.minutes}</h1>
-              <p className="font-semibold">minutes</p>
+          ) : (
+            <div className="flex flex-row gap-3 sp:w-50">
+              <div className="bg-slate-400 w-[100px] h-[150px] bg-opacity-75 flex flex-col justify-center space-y-3 items-center">
+                <h1 className="text-4xl font-bold">{remainingTime.hours}</h1>
+                <p className="font-semibold">hours</p>
+              </div>
+              <div className="bg-slate-400 w-[100px] h-[150px] bg-opacity-75 flex flex-col justify-center space-y-3 items-center">
+                <h1 className="text-4xl font-bold">{remainingTime.minutes}</h1>
+                <p className="font-semibold">minutes</p>
+              </div>
+              <div className="bg-slate-400 w-[100px] h-[150px] bg-opacity-75 flex flex-col justify-center space-y-3 items-center">
+                <h1 className="text-4xl font-bold">{remainingTime.seconds}</h1>
+                <p className="font-semibold">Seconds</p>
+              </div>
             </div>
-            <div className="bg-slate-400 w-[100px] h-[150px] bg-opacity-75 flex flex-col justify-center space-y-3 items-center">
-              <h1 className="text-4xl font-bold">{remainingTime.seconds}</h1>
-              <p className="font-semibold">Seconds</p>
-            </div>
-          </div>
+          )}
         </div>
       </div>
-      <div className="w-full md:w-[800px] p-7 my-2 md:my-12 mx-auto bg-slate-100 rounded-xl text-center">
-        <h1 className="font-bold text-xl">Any Queries?</h1>
-        <form className="flex flex-col my-3 gap-3" onSubmit={createQuery}>
-          <div className="flex flex-col md:flex-row gap-3">
-            <select
-              onChange={(e: React.ChangeEvent<HTMLSelectElement>) =>
-                handleDeskSelection(e.target.value)
-              }
-              className="p-2 rounded-lg font-semibold bg-slate-50 w-full md:w-[200px]"
-              required>
-              <option value="" selected={selectedTeam === ""}>
-                Team Name
-              </option>
-              {teamData.map((team) => (
-                <option
-                  key={team.desk}
-                  value={team.desk}
-                  selected={selectedTeam === team.desk}>
-                  {team.desk}
+      {remainingTime.hours === 0 &&
+      remainingTime.minutes === 0 &&
+      remainingTime.seconds === 0 ? (
+        <div className=" flex text-centre w-screen ">
+          <h1 className="text-6xl text-[#011A26] text-center">
+            SEE YOU ON NEXT EVENTS !
+          </h1>
+        </div>
+      ) : (
+        <div className="w-full md:w-[800px] p-7 my-2 md:my-12 mx-auto bg-slate-100 rounded-xl text-center">
+          <h1 className="font-bold text-xl">Any Queries?</h1>
+          <form className="flex flex-col my-3 gap-3" onSubmit={createQuery}>
+            <div className="flex flex-col md:flex-row gap-3">
+              <select
+                onChange={(e: React.ChangeEvent<HTMLSelectElement>) =>
+                  handleDeskSelection(e.target.value)
+                }
+                className="p-2 rounded-lg font-semibold bg-slate-50 w-full md:w-[200px]"
+                required>
+                <option value="" selected={selectedTeam === ""}>
+                  Team Name
                 </option>
-              ))}
-            </select>
-            <input
-              type="text"
-              value={teamLead}
-              disabled
-              placeholder="Team Leader Name"
-              className="p-2 rounded-lg w-full md:w-[200px] font-semibold bg-slate-50"
-            />
-            <input
-              type="text"
-              disabled
-              value={selectedTeam}
-              placeholder="Team Name"
-              className="p-2 rounded-lg w-full md:w-[200px] font-semibold bg-slate-50"
-            />
-          </div>
-          <textarea
-            value={query}
-            onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) =>
-              setQuery(e.target.value)
-            }
-            className="w-full h-[100px] placeholder:text-gray-400 placeholder:font-semibold p-2"
-            placeholder="Enter your issue/query clearly!"></textarea>
-          <button className="rounded-xl hover:bg-green-500 mx-auto hover:scale-105 bg-green-600 text-white font-bold px-2 py-2 w-full md:w-[200px] ">
-            Ask Query!
-          </button>
-        </form>
-      </div>
+                {teamData.map((team) => (
+                  <option
+                    key={team.desk}
+                    value={team.desk}
+                    selected={selectedTeam === team.desk}>
+                    {team.desk}
+                  </option>
+                ))}
+              </select>
+              <input
+                type="text"
+                value={teamLead}
+                disabled
+                placeholder="Team Leader Name"
+                className="p-2 rounded-lg w-full md:w-[200px] font-semibold bg-slate-50"
+              />
+              <input
+                type="text"
+                disabled
+                value={selectedTeam}
+                placeholder="Team Name"
+                className="p-2 rounded-lg w-full md:w-[200px] font-semibold bg-slate-50"
+              />
+            </div>
+            <textarea
+              value={query}
+              onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) =>
+                setQuery(e.target.value)
+              }
+              className="w-full h-[100px] placeholder:text-gray-400 placeholder:font-semibold p-2"
+              placeholder="Enter your issue/query clearly!"></textarea>
+            <button className="rounded-xl hover:bg-green-500 mx-auto hover:scale-105 bg-green-600 text-white font-bold px-2 py-2 w-full md:w-[200px] ">
+              Ask Query!
+            </button>
+          </form>
+        </div>
+      )}
     </main>
   );
 }
